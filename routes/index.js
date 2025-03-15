0381cae4bf1a41d6ae5c4b3b0623052cdd953a58
Luke Graham <luke.evan.graham@gmail.com>
2025-03-14 22:20:29 -0700

    working on api

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 let router = require("express").Router();
+const axios = require("axios");
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  const response = await Promise.all([
+    axios.get("https://admin.grahamwebworks.com/api/oc-clubs"),
+  ]);
+
+  // console.log("RES: ", response[0].data.data);
+  res.render("index", { clubs: response[0].data.data });
 });
 
 module.exports = router;
