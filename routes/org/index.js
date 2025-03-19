264e6d060551e9c661c73a41e8ed23e221abf669
Luke Graham <luke.evan.graham@gmail.com>
2025-03-18 23:00:21 -0700

    dev server working with sass

--- routes/org/index.js
+++ routes/org/index.js
@@ -2,9 +2,6 @@ let router = require("express").Router();
 const axios = require("axios");
 
 router.get("/:slug", async (req, res) => {
-  console.log("HERE: ", req.params.slug);
-  console.log("URL: ", process.env.API_URL);
-
   const response = await Promise.all([
     axios.get(
       `${process.env.API_URL}oc-clubs?filters[slug][$eq]=${req.params.slug}&populate=*`
